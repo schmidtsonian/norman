801f7fba57cf32c230936fad764c71b5d1acd4cd
Esteban <esteban@dorfjungs.com>
2022-06-29 13:04:51 -0500

    feat(video): aspect ratio

--- slices/SliceVideo/index.vue
+++ slices/SliceVideo/index.vue
@@ -2,7 +2,7 @@
   <section class="s-video">
     <div class="container">
       <div class="col-18 col-xl2-14 offset-xl2-2">
-        <prismicEmbed :field="slice.primary.vimeo" />
+        <prismicEmbed :field="slice.primary.vimeo" class="s-video__embeded" />
       </div>
     </div>
     <!-- <video :src="slice.primary.video.url" autoplay /> -->
@@ -21,8 +21,27 @@ export default {
 
 <style lang="scss">
   .s-video {
+    margin-bottom: 40px;
     iframe {
       width: 100%;
+      height: 100%
+    }
+  }
+
+  .s-video__embeded {
+    aspect-ratio: 16/9;
+    width: 100%;
+  }
+
+  @include breakpoint-up(bp(md)) {
+    .s-video {
+      margin-bottom: 140px;
+    }
+  }
+
+  @include breakpoint-up(bp(xl2)) {
+    .s-video {
+      margin-bottom: 240px;
     }
   }
 </style>
