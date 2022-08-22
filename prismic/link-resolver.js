049243c4e730db80e5ecd71fb425597c41faa5af
Esteban <esteban@dorfjungs.com>
2022-08-21 22:02:36 -0500

    fix(link-resolver)

--- prismic/link-resolver.js
+++ prismic/link-resolver.js
@@ -1,3 +1,12 @@
-export default function (doc) {
-  return '/'
+/**
+ * The project's Prismic Link Resolver.
+ * This function overrides the URL for a given Prismic document.
+ */
+export default function linkResolver (doc) {
+  if (doc.type === 'page' && doc.uid === 'home') {
+    return '/'
+  }
+
+  // Let Route Resolver handle all other routes.
+  return null
 }
